c8a89bb0b2381edc65711a9430fca82956386913
bosk <bosk@localhost>
2019-08-22 11:21:27 -0400

    convert Line to stateless functional component

--- src/component/Invoice/Line.js
+++ src/component/Invoice/Line.js
@@ -1,57 +1,57 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { MdCancel as DeleteIcon } from 'react-icons/md'
 
 import styles from './Line.module.scss'
 import { formatCurrency } from '../../constant/util'
 
-export default class extends Component {
-  static propTypes = {
-    index: PropTypes.number.isRequired,
-    date: PropTypes.string,
-    description: PropTypes.string,
-    quantity: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    price: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  }
+const Line = (props) => {
 
-  render() {
+  const priceFormatted = formatCurrency(props.price)
 
-    const priceFormatted = formatCurrency(this.props.price)
-
-    return (
-      <div className={`${styles.lineItem} ${styles.text}`}>
-        <div>
-          <input name="date" type="text" value={this.props.date}
-                 onChange={this.props.changeHandler(this.props.index)}
-                 readOnly={this.props.readOnly} />
-        </div>
-
-        <div>
-          <input name="description" type="text" value={this.props.description}
-                 onChange={this.props.changeHandler(this.props.index)}
-                 readOnly={this.props.readOnly} />
-        </div>
-        <div>
-          <input name="quantity" value={this.props.quantity}
-                 onChange={this.props.changeHandler(this.props.index)}
-                 readOnly={this.props.readOnly} />
-        </div>
-        <div className={styles.currency}>
-          <input name="price" value={priceFormatted}
-                 onChange={this.props.changeHandler(this.props.index)}
-                 readOnly={this.props.readOnly} />
-        </div>
-        {!this.props.readOnly ? (
-            <div style={{ borderLeft: 0 }}>
-              <button type="button"
-                      className={styles.deleteItem}
-                      onClick={this.props.deleteHandler(this.props.index)}>
-                <DeleteIcon />
-              </button>
-            </div> ) :
-          <div style={{ borderLeft: 0 }} />
-        }
+  return (
+    <div className={`${styles.lineItem} ${styles.text}`}>
+      <div>
+        <input name="date" type="text" value={props.date}
+               onChange={props.changeHandler(props.index)}
+               readOnly={props.readOnly} />
       </div>
-    )
-  }
+
+      <div>
+        <input name="description" type="text" value={props.description}
+               onChange={props.changeHandler(props.index)}
+               readOnly={props.readOnly} />
+      </div>
+      <div>
+        <input name="quantity" value={props.quantity}
+               onChange={props.changeHandler(props.index)}
+               readOnly={props.readOnly} />
+      </div>
+      <div className={styles.currency}>
+        <input name="price" value={priceFormatted}
+               onChange={props.changeHandler(props.index)}
+               readOnly={props.readOnly} />
+      </div>
+      {!props.readOnly ? (
+          <div style={{ borderLeft: 0 }}>
+            <button type="button"
+                    className={styles.deleteItem}
+                    onClick={props.deleteHandler(props.index)}>
+              <DeleteIcon />
+            </button>
+          </div> ) :
+        <div style={{ borderLeft: 0 }} />
+      }
+    </div>
+  )
+}
+
+export default Line
+
+Line.propTypes = {
+  index: PropTypes.number.isRequired,
+  date: PropTypes.string,
+  description: PropTypes.string,
+  quantity: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  price: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 }
