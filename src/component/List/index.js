e8773e7b20e3560111eb85ecdf459c0f937b9ec7
bosk <localhost>
2019-11-15 19:16:49 -0500

    case-sensitive search

--- src/component/List/index.js
+++ src/component/List/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { capitalWords, compose } from '../../constant/util'
+import { compose } from '../../constant/util'
 import { AuthUserContext } from '../Session'
 import { withFirebase } from '../Firebase'
 import Table from './Table';
@@ -41,14 +41,14 @@ class InvoicesBase extends Component {
         this.setState({ loading: true })
 
         if (query_key) {
-            const capital_query_val = capitalWords(query_val)
+            //const capital_query_val = capitalWords(query_val)
             this.props.firebase.invoices()
                 .orderByChild(query_key)
                 .limitToLast(limit)
 
                 // filter query results to substring of a child node
-                .startAt(capital_query_val)
-                .endAt(`${capital_query_val}\uf8ff`)
+                .startAt(query_val)
+                .endAt(`${query_val}\uf8ff`)
                 .once('value', snapshot => {
                     this.setInvoiceState(snapshot.val(), query_key, query_val)
                 })
