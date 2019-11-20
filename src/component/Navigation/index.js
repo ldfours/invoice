7f6d72024a113c2ea787c1d1fd6373faa3057052
bosk <localhost>
2019-11-20 18:49:33 -0500

    show menu email

--- src/component/Navigation/index.js
+++ src/component/Navigation/index.js
@@ -1,45 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-import Login from '../Login';
-import { AuthUserContext } from '../Session';
-import LogoutButton from '../Logout';
-import * as ROUTES from '../../constant/route';
+import Login from '../Login'
+import { AuthUserContext } from '../Session'
+import LogoutButton from '../Logout'
+import { SEARCH, INVOICE } from '../../constant/route'
 
 const Navigation = () => (
-  <AuthUserContext.Consumer>
-    {authUser =>
-      authUser ? (
-        <NavigationAuth authUser={authUser} />
-      ) : (
-        <NavigationNonAuth />
-      )
-    }
-  </AuthUserContext.Consumer>
-);
+    <AuthUserContext.Consumer>
+        {authUser =>
+            authUser ? (
+                <NavigationAuth authUser={authUser} />
+            ) : (
+                <NavigationNonAuth />
+            )
+        }
+    </AuthUserContext.Consumer>
+)
 
 const NavigationAuth = (props) => (
-  <nav className={"no-print"} id="page-nav">
-    {/*{JSON.stringify(props, null, 2)}*/}
-    <label htmlFor="hamburger">&#9776;</label>
-    <input type="checkbox" id="hamburger" />
-    <ul>
-      <li><Link to={ROUTES.SEARCH}>Search</Link></li>
+    <nav className={"no-print"} id="page-nav">
+        {/*{JSON.stringify(props, null, 2)}*/}
+        <label htmlFor="hamburger">&#9776;</label>
+        <input type="checkbox" id="hamburger" />
+        <ul>
+            <li><Link to={SEARCH}>Search</Link></li>
 
-      <li><Link to={{
-        pathname: ROUTES.INVOICE,
-        invoice: { readOnly: false }
-      }}>Create</Link></li>
+            <li><Link to={{
+                pathname: INVOICE,
+                invoice: { readOnly: false }
+            }}>Create</Link></li>
 
-      <li><LogoutButton /></li>
-    </ul>
-  </nav>
-);
+            <li><span style={{color: "LightGrey"}}>{props.authUser.email} </span>
+                <LogoutButton />
+            </li>
+        </ul>
+    </nav>
+)
 
 const NavigationNonAuth = () => (
-  <div className={"ui container"}>
-    <Login />
-  </div>
-);
+    <div className={"ui container"}>
+        <Login />
+    </div>
+)
 
 export default Navigation;
